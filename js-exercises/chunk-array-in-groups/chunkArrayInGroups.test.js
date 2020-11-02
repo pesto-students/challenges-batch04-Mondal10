f0a00211a4c1e7de73b099a44082a1e85bfb3f4e
Mondal10 <amitmondal111.am@gmail.com>
2020-11-03 00:30:28 +0530

    uncommenting accedental comments

--- js-exercises/chunk-array-in-groups/chunkArrayInGroups.test.js
+++ js-exercises/chunk-array-in-groups/chunkArrayInGroups.test.js
@@ -6,19 +6,19 @@ describe('chunkArrayInGroups', () => {
       ['a', 'b'],
       ['c', 'd'],
     ]);
-    // expect(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4)).toEqual([
-    //   [0, 1, 2, 3],
-    //   [4, 5],
-    // ]);
-    // expect(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3)).toEqual([
-    //   [0, 1, 2],
-    //   [3, 4, 5],
-    //   [6],
-    // ]);
-    // expect(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4)).toEqual([
-    //   [0, 1, 2, 3],
-    //   [4, 5, 6, 7],
-    //   [8],
-    // ]);
+    expect(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4)).toEqual([
+      [0, 1, 2, 3],
+      [4, 5],
+    ]);
+    expect(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3)).toEqual([
+      [0, 1, 2],
+      [3, 4, 5],
+      [6],
+    ]);
+    expect(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4)).toEqual([
+      [0, 1, 2, 3],
+      [4, 5, 6, 7],
+      [8],
+    ]);
   });
 });
